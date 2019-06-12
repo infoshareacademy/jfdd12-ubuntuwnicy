adc3210061d3b9302636ebd41ec9988942e98a41
Lucjan <lucjan.kepinski@gmail.com>
2019-06-12 22:59:44 +0200

    menu class change

--- featureListScript.js
+++ featureListScript.js
@@ -5,3 +5,21 @@ document.getElementById("signUp").addEventListener("click", function(){
     featureListEl.classList.toggle("featureListWrapperForHover")
     console.log("toggling class")
   });
+
+let hideMenu = document.querySelector('.subMenu')
+
+document.querySelector(".mobileMenu").addEventListener("click", function(){
+  hideMenu.setAttribute("class", "hideMenu")
+  hideMenu.click()
+});
+
+
+// function toggleMenu() {
+//   var menuBox = document.getElementById('menuForMobile');    
+//   if(menuBox.style.display === "flex") { // if is menuBox displayed, hide it
+//     menuBox.style.display = "none";
+//   }
+//   else { // if is menuBox hidden, display it
+//     menuBox.style.display = "flex";
+//   }
+// }
