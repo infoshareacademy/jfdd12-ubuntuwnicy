18ac1fc7547881c4697aae32e21a238cdd97c683
Jakub Karolak <karolakjakub90@gmail.com>
2019-06-13 00:19:51 +0200

    wip

--- memoryGame/scriptGame.js
+++ memoryGame/scriptGame.js
@@ -1,27 +1,32 @@
 let cards = document.querySelectorAll('.card')
 let highLightedCards = []
 let cardPairsArr = [1,2,3,4,5,6,7,8]
-let randomizedCards = randomizeCards(cardPairsArr)
-randomizedCards = randomizedCards.concat(randomizeCards(cardPairsArr))
+let randomizedCards = randomizeCardsFunc([...cardPairsArr,...cardPairsArr])
 
-function randomizeCards (cards){
+function randomizeCardsFunc (cards){
     return cards.slice().sort(() => Math.random() - 0.5)
 }
 
-
-
-
 cards.forEach(card => card.addEventListener("click", function(){
 
-    card.classList.toggle("cardsAreMatching")
-    console.log("toggling class")
+    card.classList.toggle("cardIsHighlighted")
+    console.log("toggling class to card is highlighted")
+
+
+    highLightedCards.push(card)
+
+    if (highLightedCards.length === 2){
+        if (highLightedCards[0].className === highLightedCards[1].className){
+            highLightedCards[0].classList.add('cardsAreMatching')
+            highLightedCards[1].classList.add('cardsAreMatching')
+
+        }
+    }
 }));
 
 cards.forEach((cardsElement, index) => {
 
     cardsElement.classList.add(randomizedCards[index])
-    console.log("toggling class")
+    console.log("adding card number class")
 });
-
-
 
