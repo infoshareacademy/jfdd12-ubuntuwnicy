25a21cd8f8a6b232ab7c878f479c9f0cd4539ee9
Jakub Karolak <karolakjakub90@gmail.com>
2019-06-13 09:10:39 +0200

    adding JS to handle highlighted cards, cards not matching, more than two cards selected

--- memoryGame/scriptGame.js
+++ memoryGame/scriptGame.js
@@ -1,5 +1,5 @@
 let cards = document.querySelectorAll('.card')
-let highLightedCards = []
+let highlightedCards = []
 let cardPairsArr = [1,2,3,4,5,6,7,8]
 let randomizedCards = randomizeCardsFunc([...cardPairsArr,...cardPairsArr])
 
@@ -7,26 +7,53 @@ function randomizeCardsFunc (cards){
     return cards.slice().sort(() => Math.random() - 0.5)
 }
 
-cards.forEach(card => card.addEventListener("click", function(){
-
-    card.classList.toggle("cardIsHighlighted")
-    console.log("toggling class to card is highlighted")
-
-
-    highLightedCards.push(card)
-
-    if (highLightedCards.length === 2){
-        if (highLightedCards[0].className === highLightedCards[1].className){
-            highLightedCards[0].classList.add('cardsAreMatching')
-            highLightedCards[1].classList.add('cardsAreMatching')
-
-        }
-    }
-}));
-
 cards.forEach((cardsElement, index) => {
 
     cardsElement.classList.add(randomizedCards[index])
     console.log("adding card number class")
 });
+
+cards.forEach(card => card.addEventListener("click", function(){
+
+    if (card.classList.contains('cardIsHighlighted')){
+        card.classList.toggle("cardIsHighlighted")
+        highlightedCards.pop(card)
+        console.log("toggling class to card is highlighted")
+    }else{
+
+    card.classList.toggle("cardIsHighlighted")
+    console.log("toggling class to card is highlighted")
+
+
+    highlightedCards.push(card)
+
+    if (highlightedCards.length === 2 && highlightedCards[0].className === highlightedCards[1].className){
+
+        highlightedCards[0].classList.add('cardsAreMatching')
+        highlightedCards[1].classList.add('cardsAreMatching')
+
+        highlightedCards = []
+
+        }else if(highlightedCards.length === 2){
+
+            highlightedCards[0].classList.remove('cardIsHighlighted')
+            highlightedCards[1].classList.remove('cardIsHighlighted')
+
+            highlightedCards[0].classList.add('cardsArentMatching')
+            highlightedCards[1].classList.add('cardsArentMatching')
+
+            let highlightedCardsForTimeout = []
+            highlightedCardsForTimeout = highlightedCards
+
+            setTimeout(function(){
+                highlightedCardsForTimeout[0].classList.remove('cardsArentMatching')
+                highlightedCardsForTimeout[1].classList.remove('cardsArentMatching')
+            }, 2000)
+            
+            highlightedCards = []
+        }
+    }
+}));
+
+
 
