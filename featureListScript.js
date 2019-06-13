b884d9d31bee801bb6ec12c86294cc09b8c0bc31
Lucjan Kepinski <lucjan.kepinski@gmail.com>
2019-06-13 16:54:27 +0200

    Menu onclick hide added

--- featureListScript.js
+++ featureListScript.js
@@ -1,19 +1,30 @@
 let featureListEl = document.getElementById("featureListWrapperID")
 
-document.getElementById("signUp").addEventListener("click", function(){
+document.getElementById("signUp").addEventListener("click", function () {
 
-    featureListEl.classList.toggle("featureListWrapperForHover")
-    console.log("toggling class")
-  });
-
-let hideMenu = document.querySelector('.subMenu')
-
-document.querySelector(".mobileMenu").addEventListener("click", function(){
-  hideMenu.setAttribute("class", "hideMenu")
-  hideMenu.click()
+  featureListEl.classList.toggle("featureListWrapperForHover")
+  console.log("toggling class")
 });
 
+const menuForMobile = document.getElementById("toggleMenu")
+const visibleMenus = document.getElementsByClassName('visibleMenu')
 
+menuForMobile.addEventListener("click", function () {
+  for (let index = 0; index < visibleMenus.length; index++) {
+    const visibleMenu = visibleMenus[index];
+
+    visibleMenu.classList.toggle("hideMenu")
+    console.log("toggling class")
+  }
+});
+
+// let hideMenu = document.querySelector('.subMenu')
+
+// document.querySelector(".mobileMenu").addEventListener("click", function(){
+//   hideMenu.setAttribute("class", "hideMenu")
+// });
+
+// document.getElementById("signUp").addEventListener("click",
 // function toggleMenu() {
 //   var menuBox = document.getElementById('menuForMobile');    
 //   if(menuBox.style.display === "flex") { // if is menuBox displayed, hide it
@@ -21,5 +32,20 @@ document.querySelector(".mobileMenu").addEventListener("click", function(){
 //   }
 //   else { // if is menuBox hidden, display it
 //     menuBox.style.display = "flex";
+//   }
+// }
+
+// window.nav = {
+//   status: false,
+//   menu: function(){
+//     if (nav.status){
+//       // here put class when menu is open - eg:
+//       document.getElementById("menu-box").className = "menu-box menuopen";
+//       nav.status = false;
+//     } else {
+//       // here put class when menu is closed - eg:
+//       document.getElementById("menu-box").className = "menu-box";
+//       nav.status = true;
+//     } 
 //   }
 // }
