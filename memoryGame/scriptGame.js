95999d4c892f35f80300802d0831043e747ebf9c
Jakub Karolak <karolakjakub90@gmail.com>
2019-06-17 21:53:36 +0200

    formatting the code

--- memoryGame/scriptGame.js
+++ memoryGame/scriptGame.js
@@ -37,45 +37,63 @@ const game = {
   cardsAreLoading: false
 };
 
-
-
-
-
-
-function gameLogicAndEventListener() {
-  cardsGlobal = document.querySelectorAll(".card");
-  cardsGlobal.forEach(card =>
-    card.addEventListener("click", function (event) {
-      if (game.cardsAreLoading) {
-        return;
-      }
-
-      if (card.classList.contains("cardIsHighlighted")) {
-        card.classList.toggle("cardIsHighlighted"); // checking if card is already highlighted
-        highlightedCards.pop(card);
-        console.log("toggling class to card is highlighted");
-      } else {
-        card.classList.toggle("cardIsHighlighted"); // highlighting a card and pushing to the arr
-        console.log("toggling class to card is highlighted");
-
-        highlightedCards.push(card);
-
-        if (highlightedCards.length === 2) {
-          if (highlightedCards[0].className === highlightedCards[1].className) {
-            cardsAreMatching();
-          } else {
-            cardsArentMatching();
-          }
-          game.cardsAreLoading = true;
-          setTimeout(() => (game.cardsAreLoading = false), timeoutLength);
-        }
-      }
-    })
-  );
-}
-
 function randomizeCardsFunc(cards) {
   return cards.slice().sort(() => Math.random() - 0.5);
+}
+
+function randomizePairs() {
+  randomizedCards = randomizeCardsFunc(cardPairsArr)
+  randomizedCardsHard = randomizeCardsFunc(cardPairsArrHard)
+}
+
+function initializeGame() {
+  let cards = document.querySelectorAll(".card")
+  if (isHardModeOn) {
+    isHardModeOn = false;
+    let gridContainer = document.querySelector(".gridContainerHard");
+    gridContainer.classList.remove("gridContainerHard");
+    gridContainer.classList.add("gridContainer");
+    cards.forEach(cardsElement => {
+      cardsElement.remove();
+    });
+  } cards.forEach(cardsElement => {
+    cardsElement.remove();
+  });
+  let gridContainer = document.querySelector(".gridContainer");
+
+  let divs = document.createElement("div");
+  randomizePairs()
+  for (let i = 0; i < 16; i++) {
+    console.log(i);
+    divs = document.createElement("div");
+    divs.classList.add("card");
+    divs.classList.add(randomizedCards[i]);
+    gridContainer.appendChild(divs);
+  }
+  cardsGlobal = document.querySelectorAll(".card");
+  gameLogicAndEventListener();
+}
+
+function toggleHardMode() {
+  cards = document.querySelectorAll(".card");
+
+  isHardModeOn = true;
+  let gridContainer = document.querySelector(".gridContainer");
+  gridContainer.classList.remove("gridContainer");
+  gridContainer.classList.add("gridContainerHard");
+  cards.forEach(cardsElement => {
+    cardsElement.remove();
+  });
+
+  randomizePairs()
+  for (let i = 0; i < 36; i++) {
+    console.log(i);
+    divs = document.createElement("div");
+    divs.classList.add("card");
+    divs.classList.add(randomizedCardsHard[i]);
+    gridContainer.appendChild(divs);
+  }
+  gameLogicAndEventListener();
 }
 
 function addRemoveCardsClass(
@@ -129,57 +147,41 @@ function cardsArentMatching() {
   highlightedCards = [];
 }
 
-function toggleHardMode() {
-  cards = document.querySelectorAll(".card");
-
-  isHardModeOn = true;
-  let gridContainer = document.querySelector(".gridContainer");
-  gridContainer.classList.remove("gridContainer");
-  gridContainer.classList.add("gridContainerHard");
-  cards.forEach(cardsElement => {
-    cardsElement.remove();
-  });
-  
-  randomizePairs()
-  for (let i = 0; i < 36; i++) {
-    console.log(i);
-    divs = document.createElement("div");
-    divs.classList.add("card");
-    divs.classList.add(randomizedCardsHard[i]);
-    gridContainer.appendChild(divs);
-  }
-  gameLogicAndEventListener();
-}
-
-function initializeGame() {
-  let cards = document.querySelectorAll(".card")
-  if (isHardModeOn) {
-    isHardModeOn = false;
-    let gridContainer = document.querySelector(".gridContainerHard");
-    gridContainer.classList.remove("gridContainerHard");
-    gridContainer.classList.add("gridContainer");
-    cards.forEach(cardsElement => {
-      cardsElement.remove();
-    });
-  }cards.forEach(cardsElement => {
-    cardsElement.remove();
-  });
-  let gridContainer = document.querySelector(".gridContainer");
-
-  let divs = document.createElement("div");
-  randomizePairs()
-  for (let i = 0; i < 16; i++) {
-    console.log(i);
-    divs = document.createElement("div");
-    divs.classList.add("card");
-    divs.classList.add(randomizedCards[i]);
-    gridContainer.appendChild(divs);
-  }
+function gameLogicAndEventListener() {
   cardsGlobal = document.querySelectorAll(".card");
-  gameLogicAndEventListener();
+  cardsGlobal.forEach(card =>
+    card.addEventListener("click", function (event) {
+      if (game.cardsAreLoading) {
+        return;
+      }
+
+      if (card.classList.contains("cardIsHighlighted")) {
+        card.classList.toggle("cardIsHighlighted"); // checking if card is already highlighted
+        highlightedCards.pop(card);
+        console.log("toggling class to card is highlighted");
+      } else {
+        card.classList.toggle("cardIsHighlighted"); // highlighting a card and pushing to the arr
+        console.log("toggling class to card is highlighted");
+
+        highlightedCards.push(card);
+
+        if (highlightedCards.length === 2) {
+          if (highlightedCards[0].className === highlightedCards[1].className) {
+            cardsAreMatching();
+          } else {
+            cardsArentMatching();
+          }
+          game.cardsAreLoading = true;
+          setTimeout(() => (game.cardsAreLoading = false), timeoutLength);
+        }
+      }
+    })
+  );
 }
 
-function randomizePairs(){
-  randomizedCards = randomizeCardsFunc(cardPairsArr)
-  randomizedCardsHard = randomizeCardsFunc(cardPairsArrHard)
-}
+
+
+
+
+
+
