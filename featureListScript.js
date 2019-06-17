1af5b77169bfea85ca63244ac4fd4028af05e98c
Lucjan Kepinski <lucjan.kepinski@gmail.com>
2019-06-17 15:41:46 +0200

    instruction and difficulty buttons positioned, background for both windows added

--- featureListScript.js
+++ featureListScript.js
@@ -17,35 +17,3 @@ menuForMobile.addEventListener("click", function () {
     console.log("toggling class")
   }
 });
-
-// let hideMenu = document.querySelector('.subMenu')
-
-// document.querySelector(".mobileMenu").addEventListener("click", function(){
-//   hideMenu.setAttribute("class", "hideMenu")
-// });
-
-// document.getElementById("signUp").addEventListener("click",
-// function toggleMenu() {
-//   var menuBox = document.getElementById('menuForMobile');    
-//   if(menuBox.style.display === "flex") { // if is menuBox displayed, hide it
-//     menuBox.style.display = "none";
-//   }
-//   else { // if is menuBox hidden, display it
-//     menuBox.style.display = "flex";
-//   }
-// }
-
-// window.nav = {
-//   status: false,
-//   menu: function(){
-//     if (nav.status){
-//       // here put class when menu is open - eg:
-//       document.getElementById("menu-box").className = "menu-box menuopen";
-//       nav.status = false;
-//     } else {
-//       // here put class when menu is closed - eg:
-//       document.getElementById("menu-box").className = "menu-box";
-//       nav.status = true;
-//     } 
-//   }
-// }
