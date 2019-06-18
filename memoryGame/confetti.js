5fca9dc4b200a298747e870b51fd652590edf8d2
Marta Kirschenstein <kirschenstein.marta@gmail.com>
2019-06-18 15:35:03 +0200

    effect of drawing functions checked

--- memoryGame/confetti.js
+++ memoryGame/confetti.js
@@ -8,6 +8,10 @@ canvas.setAttribute('height', '800');
 
 const ctx = canvas.getContext('2d');
 
+function degresForCircle(deg) {
+    return deg * Math.PI/180;
+}
+
 function drawRect (x, y, width, height, color = 'blue') {
     ctx.fillStyle = color
     ctx.fillRect(x, y, width, height)
@@ -17,16 +21,15 @@ function drawTriangle (x, y, color = 'blue') {
     ctx.beginPath()
     ctx.moveTo(x, y)
     ctx.lineTo(x, y)
-    ctx.moveTo(x, y)
     ctx.lineTo(x, y)
     ctx.closePath()
     ctx.fillStyle = color
     ctx.fill()
 }
 
-function drawCircle (x, y, r, start, stop, color = 'blue') {
+function drawCircle (x, y, r, start, degresForCircle, color = 'blue') {
     ctx.beginPath();
-    ctx.arc(x, y, r, start, stop)
-    ctx.closePath();
+    ctx.arc(x, y, r, start, degresForCircle(deg))
     ctx.fillStyle = color
+    ctx.fill()
 }
