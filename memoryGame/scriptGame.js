ff3a8f2fe7b0f12fe1354eddb6c4ef3be030bd2b
Jakub K <karolakjakub90@gmail.com>
2019-06-18 14:36:39 +0200

    changing cursor style for form, setting up the form so it will redirect the user to game html

--- memoryGame/scriptGame.js
+++ memoryGame/scriptGame.js
@@ -27,9 +27,9 @@ let randomizedCards = randomizeCardsFunc(cardPairsArr);
 let randomizedCardsHard = randomizeCardsFunc(cardPairsArrHard);
 let timeoutLength = 3000;
 let howManyPairsLeft = 8;
-let cardsGlobal
+let cardsGlobal;
 
-initializeGame();
+initializeGame(); // initializing game for the first time
 
 cardsGlobal = document.querySelectorAll(".card");
 
@@ -42,12 +42,12 @@ function randomizeCardsFunc(cards) {
 }
 
 function randomizePairs() {
-  randomizedCards = randomizeCardsFunc(cardPairsArr)
-  randomizedCardsHard = randomizeCardsFunc(cardPairsArrHard)
+  randomizedCards = randomizeCardsFunc(cardPairsArr);
+  randomizedCardsHard = randomizeCardsFunc(cardPairsArrHard);
 }
 
 function initializeGame() {
-  let cards = document.querySelectorAll(".card")
+  let cards = document.querySelectorAll(".card");
   if (isHardModeOn) {
     isHardModeOn = false;
     let gridContainer = document.querySelector(".gridContainerHard");
@@ -56,13 +56,14 @@ function initializeGame() {
     cards.forEach(cardsElement => {
       cardsElement.remove();
     });
-  } cards.forEach(cardsElement => {
+  }
+  cards.forEach(cardsElement => {
     cardsElement.remove();
   });
   let gridContainer = document.querySelector(".gridContainer");
 
   let divs = document.createElement("div");
-  randomizePairs()
+  randomizePairs();
   for (let i = 0; i < 16; i++) {
     console.log(i);
     divs = document.createElement("div");
@@ -85,7 +86,7 @@ function toggleHardMode() {
     cardsElement.remove();
   });
 
-  randomizePairs()
+  randomizePairs();
   for (let i = 0; i < 36; i++) {
     console.log(i);
     divs = document.createElement("div");
@@ -108,6 +109,38 @@ function addRemoveCardsClass(
     cardsArr[0].classList.remove(cardsClass);
     cardsArr[1].classList.remove(cardsClass);
   }
+}
+
+function gameLogicAndEventListener() {
+  cardsGlobal = document.querySelectorAll(".card");
+  cardsGlobal.forEach(card =>
+    card.addEventListener("click", function(event) {
+      if (game.cardsAreLoading) {
+        return;
+      }
+
+      if (card.classList.contains("cardIsHighlighted")) {
+        card.classList.toggle("cardIsHighlighted"); // checking if card is already highlighted
+        highlightedCards.pop(card);
+        console.log("toggling class to card is highlighted");
+      } else {
+        card.classList.toggle("cardIsHighlighted"); // highlighting a card and pushing to the arr
+        console.log("toggling class to card is highlighted");
+
+        highlightedCards.push(card);
+
+        if (highlightedCards.length === 2) {
+          if (highlightedCards[0].className === highlightedCards[1].className) {
+            cardsAreMatching();
+          } else {
+            cardsArentMatching();
+          }
+          game.cardsAreLoading = true;
+          setTimeout(() => (game.cardsAreLoading = false), timeoutLength);
+        }
+      }
+    })
+  );
 }
 
 function cardsAreMatching() {
@@ -146,42 +179,3 @@ function cardsArentMatching() {
 
   highlightedCards = [];
 }
-
-function gameLogicAndEventListener() {
-  cardsGlobal = document.querySelectorAll(".card");
-  cardsGlobal.forEach(card =>
-    card.addEventListener("click", function (event) {
-      if (game.cardsAreLoading) {
-        return;
-      }
-
-      if (card.classList.contains("cardIsHighlighted")) {
-        card.classList.toggle("cardIsHighlighted"); // checking if card is already highlighted
-        highlightedCards.pop(card);
-        console.log("toggling class to card is highlighted");
-      } else {
-        card.classList.toggle("cardIsHighlighted"); // highlighting a card and pushing to the arr
-        console.log("toggling class to card is highlighted");
-
-        highlightedCards.push(card);
-
-        if (highlightedCards.length === 2) {
-          if (highlightedCards[0].className === highlightedCards[1].className) {
-            cardsAreMatching();
-          } else {
-            cardsArentMatching();
-          }
-          game.cardsAreLoading = true;
-          setTimeout(() => (game.cardsAreLoading = false), timeoutLength);
-        }
-      }
-    })
-  );
-}
-
-
-
-
-
-
-
