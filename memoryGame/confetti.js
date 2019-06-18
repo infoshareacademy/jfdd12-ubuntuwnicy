5e2123d3473e7efeed033a389925fec66a2598ea
Marta Kirschenstein <kirschenstein.marta@gmail.com>
2019-06-18 16:25:28 +0200

    triangles in random places on canvas

--- memoryGame/confetti.js
+++ memoryGame/confetti.js
@@ -12,20 +12,33 @@ function degresForCircle(deg) {
     return deg * Math.PI/180;
 }
 
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min
+}
+
+let height = 10 * (Math.sqrt(3)/2);
+let x = randomNumber(0, 800)
+let y = randomNumber(0, 800)
+//let c = a - randomNumber(0, 5)
+//let d = a - randomNumber(0, 5)
+
 function drawRect (x, y, width, height, color = 'blue') {
     ctx.fillStyle = color
     ctx.fillRect(x, y, width, height)
 }
 
+
 function drawTriangle (x, y, color = 'blue') {
     ctx.beginPath()
     ctx.moveTo(x, y)
-    ctx.lineTo(x, y)
-    ctx.lineTo(x, y)
+    ctx.lineTo(x + 5, y + height)
+    ctx.lineTo(x - 5, y + height)
     ctx.closePath()
     ctx.fillStyle = color
     ctx.fill()
 }
+
+drawTriangle(x, y)
 
 function drawCircle (x, y, r, start, degresForCircle, color = 'blue') {
     ctx.beginPath();
