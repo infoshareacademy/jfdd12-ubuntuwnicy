b993c72e32e9a58dfc00618e5522a4fbe0f5669a
Marta Kirschenstein <kirschenstein.marta@gmail.com>
2019-06-18 16:52:18 +0200

    all elements added

--- memoryGame/confetti.js
+++ memoryGame/confetti.js
@@ -8,6 +8,20 @@ canvas.setAttribute('height', '800');
 
 const ctx = canvas.getContext('2d');
 
+
+let heightOfTriangle = 12 * (Math.sqrt(3)/2);
+let x = randomNumber(0, 800)
+let y = randomNumber(0, 800)
+let rectX = randomNumber(0, 800)
+let rectY = randomNumber(0, 800)
+let circleX = randomNumber(0, 800)
+let circleY = circleX
+let widthOfRect = 10
+let heightOfRect = 10
+let r = 6
+let start = 0
+
+
 function degresForCircle(deg) {
     return deg * Math.PI/180;
 }
@@ -16,23 +30,19 @@ function randomNumber(min, max) {
     return Math.random() * (max - min) + min
 }
 
-let height = 10 * (Math.sqrt(3)/2);
-let x = randomNumber(0, 800)
-let y = randomNumber(0, 800)
-//let c = a - randomNumber(0, 5)
-//let d = a - randomNumber(0, 5)
 
-function drawRect (x, y, width, height, color = 'blue') {
+function drawRect (rectX, rectY, widthOfRect, heightOfRect, color = 'blue') {
     ctx.fillStyle = color
-    ctx.fillRect(x, y, width, height)
+    ctx.fillRect(rectX, rectY, widthOfRect, heightOfRect)
 }
 
+drawRect(rectX, rectY, widthOfRect, heightOfRect)
 
 function drawTriangle (x, y, color = 'blue') {
     ctx.beginPath()
     ctx.moveTo(x, y)
-    ctx.lineTo(x + 5, y + height)
-    ctx.lineTo(x - 5, y + height)
+    ctx.lineTo(x + 6, y + heightOfTriangle)
+    ctx.lineTo(x - 6, y + heightOfTriangle)
     ctx.closePath()
     ctx.fillStyle = color
     ctx.fill()
@@ -40,9 +50,11 @@ function drawTriangle (x, y, color = 'blue') {
 
 drawTriangle(x, y)
 
-function drawCircle (x, y, r, start, degresForCircle, color = 'blue') {
+function drawCircle (circleX, circleY, r, start, degresForCircle, color = 'blue') {
     ctx.beginPath();
-    ctx.arc(x, y, r, start, degresForCircle(deg))
+    ctx.arc(circleX, circleY, r, start, 2 * Math.PI)
     ctx.fillStyle = color
     ctx.fill()
 }
+
+drawCircle(circleX, circleY, r, start)
