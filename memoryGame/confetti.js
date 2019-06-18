580dfbc83369713d5915dabb66bc6c4cc65e3a6d
Marta Kirschenstein <kirschenstein.marta@gmail.com>
2019-06-18 15:15:54 +0200

    functions for drawing rect, triangle and circle added

--- memoryGame/confetti.js
+++ memoryGame/confetti.js
@@ -1,4 +1,32 @@
 
-const canvas = document.getElementById('#can')
+const canvas = document.createElement('canvas');
 
+body.append(canvas);
 
+canvas.setAttribute('width', '800');
+canvas.setAttribute('height', '800');
+
+const ctx = canvas.getContext('2d');
+
+function drawRect (x, y, width, height, color = 'blue') {
+    ctx.fillStyle = color
+    ctx.fillRect(x, y, width, height)
+}
+
+function drawTriangle (x, y, color = 'blue') {
+    ctx.beginPath()
+    ctx.moveTo(x, y)
+    ctx.lineTo(x, y)
+    ctx.moveTo(x, y)
+    ctx.lineTo(x, y)
+    ctx.closePath()
+    ctx.fillStyle = color
+    ctx.fill()
+}
+
+function drawCircle (x, y, r, start, stop, color = 'blue') {
+    ctx.beginPath();
+    ctx.arc(x, y, r, start, stop)
+    ctx.closePath();
+    ctx.fillStyle = color
+}
