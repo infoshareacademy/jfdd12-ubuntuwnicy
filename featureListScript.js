854ea62456fbd2f10d046daab8fa9614660a7df9
Jakub K <karolakjakub90@gmail.com>
2019-06-19 15:37:06 +0200

    adding form buttons functionality

--- featureListScript.js
+++ featureListScript.js
@@ -3,12 +3,14 @@ const menuForMobile = document.getElementById("toggleMenu");
 const visibleMenus = document.getElementsByClassName("visibleMenu");
 const formSubmitButton = document.getElementById("submitButton");
 
-document.getElementById("signUp").addEventListener("click", function() {
+let backOutButton = document.querySelector('.backOutButton')
+
+document.getElementById("signUp").addEventListener("click", function () {
   featureListEl.classList.toggle("featureListWrapperForHover");
   console.log("toggling class");
 });
 
-menuForMobile.addEventListener("click", function() {
+menuForMobile.addEventListener("click", function () {
   for (let index = 0; index < visibleMenus.length; index++) {
     const visibleMenu = visibleMenus[index];
 
@@ -17,18 +19,24 @@ menuForMobile.addEventListener("click", function() {
   }
 });
 
-formSubmitButton.addEventListener("click", function() {
+formSubmitButton.addEventListener("click", function () {
   if (checkIfFormIsValid()) {
     let formPopUpBackground = document.querySelector(".formPopUpBackgroundOff");
 
     formPopUpBackground.classList.remove("formPopUpBackgroundOff");
     formPopUpBackground.classList.add("formPopUpBackground");
-    
-    let toGameButton = document.querySelector('.toGameButton')
-    let backOutButton = 
-//TODO
+
   }
 });
+
+backOutButton.addEventListener('click', function () {
+
+  let formPopUpBackground = document.querySelector(".formPopUpBackground")
+
+  formPopUpBackground.classList.remove("formPopUpBackground")
+  formPopUpBackground.classList.add("formPopUpBackgroundOff")
+
+})
 
 function checkIfFormIsValid() {
   return document.getElementById("emailForm").checkValidity();
