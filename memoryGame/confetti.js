e50f8458bbabbd1419d5191958b8f13d72378d85
Marta Kirschenstein <kirschenstein.marta@gmail.com>
2019-06-21 16:53:44 +0200

    starting multiply elements

--- memoryGame/confetti.js
+++ memoryGame/confetti.js
@@ -10,53 +10,62 @@ const ctx = canvas.getContext('2d');
 
 
 const heightOfTriangle = 12 * (Math.sqrt(3)/2);
-const x = 400
-const y = 790
-const widthOfRect = 10
+const xRect = getRandomNumber(0, 800)
+const yRect = getRandomNumber(0, 12)
+const xTriangle = getRandomNumber(0, 800)
+const yTriangle = getRandomNumber(0, 12)
+const xCircle = getRandomNumber(0, 800)
+const yCircle = getRandomNumber(0, 12)
+const widthOfRect = 10  
 const heightOfRect = 10
 const r = 6
 const start = 0
 
 
-function randomNumber(min, max) {
+function getRandomNumber(min, max) {
     return Math.random() * (max - min) + min
 }
 
-function randomColor() {
-    const red = randomNumber(0, 255)
-    const green = randomNumber(0, 255)
-    const blue = randomNumber(0, 255)
+function getRandomColor() {
+    const red = getRandomNumber(0, 255)
+    const green = getRandomNumber(0, 255)
+    const blue = getRandomNumber(0, 255)
 
     return `rgb(${red}, ${green}, ${blue})`
 }
 
 
-function drawRect (x, y, widthOfRect, heightOfRect) {
-    ctx.fillStyle = randomColor()
-    ctx.fillRect(x, y, widthOfRect, heightOfRect)
+function drawRect (xRect, yRect, widthOfRect, heightOfRect) {
+    ctx.fillStyle = getRandomColor()
+    ctx.fillRect(xRect, yRect, widthOfRect, heightOfRect)
 }
 
-function drawTriangle (x, y) {
+function drawTriangle (xTriangle, yTriangle) {
     ctx.beginPath()
-    ctx.moveTo(x, y)
-    ctx.lineTo(x + 6, y + heightOfTriangle)
-    ctx.lineTo(x - 6, y + heightOfTriangle)
+    ctx.moveTo(xTriangle, yTriangle)
+    ctx.lineTo(xTriangle + 6, yTriangle + heightOfTriangle)
+    ctx.lineTo(xTriangle - 6, yTriangle + heightOfTriangle)
     ctx.closePath()
-    ctx.fillStyle = randomColor()
+    ctx.fillStyle = getRandomColor()
     ctx.fill()
 }
 
-function drawCircle (x, y, r, start) {
+function drawCircle (xCircle, yCircle, r, start) {
     ctx.beginPath();
-    ctx.arc(x, y, r, start, 2 * Math.PI)
-    ctx.fillStyle = randomColor()
+    ctx.arc(xCircle, yCircle, r, start, 2 * Math.PI)
+    ctx.fillStyle = getRandomColor()
     ctx.fill()
 }
 
 function confettiExplode () {
-    drawRect(x, y, widthOfRect, heightOfRect)
-    drawTriangle(x, y)
-    drawCircle(x, y, r, start)
+    drawRect(xRect, yRect, widthOfRect, heightOfRect)
+    drawTriangle(xTriangle, yTriangle)
+    drawCircle(xCircle, yCircle, r, start)
 }
 
 confettiExplode()
+confettiExplode()
+confettiExplode()
+confettiExplode()
+confettiExplode()
+
